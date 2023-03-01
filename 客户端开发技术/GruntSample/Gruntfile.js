023fde45f7de465484ef77849e02abcd4ef2c1c0
意 张 <z1494934626@outlook.com>
2023-03-01 10:25:07 +0800

    客户端开发技术

---
+++ 客户端开发技术/GruntSample/Gruntfile.js
@@ -0,0 +1,14 @@
+﻿module.exports = function (grunt) {
+    grunt.initConfig({
+        /*删除文件*/
+        clean: ["wwwroot/lib/*"],
+        /*合并*/
+        //concat: {
+        //    all: {
+        //        src:['']
+        //    }
+        //}
+    })
+    //grunt.registerTask("all", ['clean'])
+    grunt.registerTask("clean")
+}
